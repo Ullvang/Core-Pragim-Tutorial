33648fac0e381e2d89b988b4696efafffebc7578
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2020-04-16 19:44:16 +0200

    added details action in homecontroller

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -19,5 +19,11 @@ namespace EmployeeManagement.Controllers
         {
             return _employeeRepository.GetEmployee(1).Name;
         }
+
+        public ViewResult Details()
+        {
+            Employee model = _employeeRepository.GetEmployee(1);
+            return View(model);
+        }
     }
 }
