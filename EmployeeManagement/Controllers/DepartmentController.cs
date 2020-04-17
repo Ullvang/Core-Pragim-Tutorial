b1391b94805bfcd8da56201d9c25635802a8c8d1
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2020-04-17 23:35:30 +0200

    routing testing

---
+++ EmployeeManagement/Controllers/DepartmentController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public string List()
+        {
+            return "Dept-controller List-action";
+        }
+
+        public string Details()
+        {
+            return "Dept-controller Details-action";
+        }
+    }
+}
