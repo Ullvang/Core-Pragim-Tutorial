3e0fe101d9829def887b81728f942c2a39eaa4a6
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2020-04-27 18:51:36 +0200

    changed password complexity

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -29,7 +29,11 @@ namespace EmployeeManagement
         {
             services.AddDbContextPool<AppDbContext>(options => options.UseSqlServer(_config.GetConnectionString("EmployeeDBConnection")));
 
-            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                options.Password.RequiredLength = 10;
+                options.Password.RequiredLength = 3;
+            }).AddEntityFrameworkStores<AppDbContext>();
 
             services.AddMvc(options => options.EnableEndpointRouting = false).AddXmlSerializerFormatters();
             services.AddScoped<IEmployeeRepository, SQLEmployeeRepository>();
