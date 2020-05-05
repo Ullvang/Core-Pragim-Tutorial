c557b307e4b4296af57e7350910b2400fcfebdd4
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2020-05-05 20:04:58 +0200

    encrypted and decrypted id

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -62,6 +62,8 @@ namespace EmployeeManagement
             services.AddScoped<IEmployeeRepository, SQLEmployeeRepository>();
             services.AddSingleton<IAuthorizationHandler, CanEditOnlyOtherAdminRolesAndClaimsHandler>();
             services.AddSingleton<IAuthorizationHandler, SuperAdminHandler>();
+            services.AddSingleton<DataProtectionPurposeStrings>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
